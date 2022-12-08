910d4c87db067ed12df62141285fce0e7cb66d8c
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2022-12-08 13:06:04 +0100

    aplikace

--- src/components/OneTask.js
+++ src/components/OneTask.js
@@ -4,7 +4,7 @@ const OneTask = (props) => {
     return (
         <div 
         classname="task"
-        style={{backgroundColor: props.completed ? "green" : "rgb(99, 73, 123)"}}
+        style={{width: "500px", margin: "0 auto", backgroundColor: props.completed ? "green" : "rgb(99, 73, 123)"}}
         >
           <button className="btncomplete" onClick={() => props.completeTask(props.id)}>hotovo</button>
           <button className="btndelete" onClick={() => props.deleteTask(props.id)}> Koupeno </button>         
